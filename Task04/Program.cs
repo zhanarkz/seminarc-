f809df4552648800f689104f9afa387577cc398c
Zhanar <zhanarkz@gmail.com>
2022-10-04 21:55:43 +0600

    after check

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -13,9 +13,9 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Write number3");
 int number3 = Convert.ToInt32(Console.ReadLine());
 
-int max = number1;
+int max = number1; 
 
-if (number1 > max ) max = number1;
+//if (number1 > max ) max = number1; - лишнее
 if (number2 > max ) max = number2;
 if (number3 > max ) max = number3;
 
