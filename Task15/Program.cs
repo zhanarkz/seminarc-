5640652cb5fb0b7152cc72cf283d702d28531660
Zhanar <zhanarkz@gmail.com>
2022-10-07 23:03:30 +0600

    Дз после проверки

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -10,5 +10,5 @@ Console.WriteLine("Введите номер дня недели: ");
 int day = Convert.ToInt32(Console.ReadLine());
 
 if (day == 6 || day == 7) { Console.WriteLine("Да, это выходной!"); }
-else if (day == 1 || day == 2 || day == 3 || day == 4 || day == 5) { Console.WriteLine("Нет, это не выходной день"); }
+else if (day >= 1 && day <= 5) Console.WriteLine("Нет, это не выходной день");
 else Console.WriteLine("Нет такого дня");
