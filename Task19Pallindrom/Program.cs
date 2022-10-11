f19d3ed32a5ca21bfa59824beefc972d52f3c3fd
Zhanar <zhanarkz@gmail.com>
2022-10-11 23:34:52 +0600

    Right Pallindrome

--- Task19Pallindrom/Program.cs
+++ Task19Pallindrom/Program.cs
@@ -8,15 +8,15 @@
 // 23432 -> да
 System.Console.WriteLine ("Введите пятизначное число:  ");
 int N = Convert.ToInt32(Console.ReadLine());
-bool CheckPallindrome (int num)
+bool checkPallindrome (int num)
 {
     int firstDigit = num / 10000; 
-    int secondDigit = num % 10000; 
+    int secondDigit = num % 10000 / 1000; 
     int forthDigit = num % 100 / 10;
     int lastDigit = num % 10;
-    return lastDigit == firstDigit || secondDigit == forthDigit;
+    return lastDigit == firstDigit && secondDigit == forthDigit;
 }
 
-if (CheckPallindrome(N)) Console.Write("да");
+if (checkPallindrome(N)) Console.Write("да");
 else if (N > 99999 || N < 10000) Console.Write("не пятизначное число");
 else Console.Write("нет");
