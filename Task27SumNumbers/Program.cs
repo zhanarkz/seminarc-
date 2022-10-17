ee6b2f2123b05416a85a3a69339190ff305c7060
Zhanar <zhanarkz@gmail.com>
2022-10-17 18:41:35 +0600

    ДЗ 4го урока

---
+++ Task27SumNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая 
+// принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+SumNumbers(number);
+
+void SumNumbers(int num)
+{
+int sum = 0;
+while (num > 0)
+{
+int numb = num % 10;
+num = num / 10;
+sum = sum + numb;
+}
+Console.WriteLine($"сумма всех цифр в числе равна: {sum}");
+}
