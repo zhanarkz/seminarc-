060b219e8a9e66799ae8c99f9c6b619ef58494e8
Zhanar <zhanarkz@gmail.com>
2022-11-09 22:52:34 +0600

    HW 7 seminar

---
+++ Task50/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 50. Напишите программу, которая 
+// на вход принимает позиции элемента в двумерном массиве, и 
+// возвращает значение этого элемента или 
+// же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого элемента в массиве нет
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    var matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j],4}, ");
+            else Console.Write($"{array[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void SearchElement(int[,] array, int a, int b)
+{
+
+            if (a >= 0 && a < array.GetLength(0) && b >= 0 && b < array.GetLength(1))
+            {
+                Console.WriteLine(array[a, b]);
+            }
+            else Console.WriteLine("такого элемента в массиве нет");
+}
+
+Console.Write("Введите строку: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите столбец: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(matrix);
+
+SearchElement(matrix, row, column);
